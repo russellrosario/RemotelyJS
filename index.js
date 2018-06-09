e8c9422e34adbda9b3bbe57149db1546c5341480
Russell Rosario <rosariorussell@gmail.com>
2018-06-08 20:57:01 -0400

    added google authentication

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+
+}))
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
