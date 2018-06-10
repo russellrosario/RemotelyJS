ee2dc612507ed5c3b256b5e919b139f3a0c8a260
Russell Rosario <rosariorussell@gmail.com>
2018-06-10 14:42:28 -0400

    Added google authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,16 @@ module.exports = (app) => {
     scope: ['profile', 'email']
   }))
 
-  app.get('/auth/google/callback', passport.authenticate('google'))
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.send('you are logged in!')
+  })
+
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+  })
 }
--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,40 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
+// mongoose models are not exported using CommonJS
+const User = mongoose.model('users')
+
+// serialize cookie before sending in header
+// null is error object
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// deserialize cookie to authenticate user
+// null is error object
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+})
+
+// passport.authenticate('google')
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser)
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user))
+      }
+    })
 }))
