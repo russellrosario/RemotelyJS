48513a625f62e23cec119ce7c8583f9f340248b5
Russell Rosario <rosariorussell@gmail.com>
2018-06-14 04:48:11 -0400

    fixed typo

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-if (process.env === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   const path = require('path')
   app.get('*', (req, res) => {
