72ac4afc2dc8b2ce03136c8f53a49de153d7d4c9
Russell Rosario <rosariorussell@gmail.com>
2018-06-15 01:59:05 -0400

    added surveys

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 const bodyParser = require('body-parser')
 const keys = require('./config/keys')
 require('./models/User')
+require('./models/Survey')
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
@@ -24,6 +25,7 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+require('./routes/surveyRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
