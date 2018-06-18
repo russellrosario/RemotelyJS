e436eb37f87ef11c93bef0648cfda1f5b04afa24
Russell Rosario <rosariorussell@gmail.com>
2018-06-18 16:29:01 -0400

    completed boilerplate code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,27 @@
-import axios from 'axios'
-import { FETCH_USER } from './types'
+import axios from 'axios';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
-// export const fetchUser = () => async dispatch => dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') })
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user')
+  const res = await axios.get('/api/current_user');
 
-  dispatch({ type: FETCH_USER, payload: res.data })
-}
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
 
-export const handleToken = (token) => async dispatch => {
-  const res = await axios.post('/api/stripe', token)
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
 
-  dispatch({ type: FETCH_USER, payload: res.data })
-}
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,29 +5,29 @@ import * as actions from '../actions'
 
 import Header from './Header'
 import Landing from './Landing'
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
+import Dashboard from './Dashboard'
+import SurveyNew from './surveys/SurveyNew'
 
 class App extends Component {
-  componentDidMount() {
-    this.props.fetchUser()
-  }
+    componentDidMount() {
+        this.props.fetchUser()
+    }
 
-  render() {
-    return (
-      <div className="container" >
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+    render() {
+        return (
+            <div className="container" >
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
 
-          </div>
-        </BrowserRouter>
-      </div>
-    )
-  }
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
 
 export default connect(null, actions)(App)
