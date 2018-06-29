eac198161c4b23ed414f222cebfe17c826557b0c
Mario Ricart <mario.r69@gmail.com>
2018-06-29 19:27:03 -0400

    added sorting date

--- routes/jobBoardRoutes.js
+++ routes/jobBoardRoutes.js
@@ -9,16 +9,19 @@ const scraper = require('../controllers/jobBoard');
 module.exports = app => {
   
     app.get('/api/jobs/list', async (req, res) => {
-        const results = await Jobs.find();
+        //sorted last scraped shown first
+        const results = await Jobs.find().sort({'dateAdded': -1});
         res.json(results);
     });
 
     app.get('/api/jobs/match/:tag', async (req,res)=>{
         const results = await Jobs.find({ 
             $or :[
+                //if the jobTitle *or* description contains tag
+                //sorted last scraped shown first
                 {'jobTitle': { $regex: new RegExp(req.params.tag, 'gi')}},
                 {'description': { $regex: new RegExp(req.params.tag, 'gi')}}
-             ]});
+             ]}).sort({'dateAdded': -1});
 
              console.log(results);
         res.json(results);
